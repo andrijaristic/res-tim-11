29e08d558d5f7bccf8d4fb92f69bd6611ae01328
jovan12121 <jovanmiladinovice14@gmail.com>
2022-05-29 12:04:55 +0200

    popravka bagova

--- projekat/worker/classes.py
+++ projekat/worker/classes.py
@@ -6,27 +6,22 @@ class Worker:
         self.address = address
         self.databasecrud_address = databasecrud_address
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #self.server_socket.bind(self.address)
-        #self.server_socket.listen(1)
         self.server_socket.connect(address)
         self.database_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.database_socket.bind(self.databasecrud_address)
-        self.database_socket.listen(1)
-        #self.database_socket.connect(databasecrud_address)
-
+        self.database_socket.connect(databasecrud_address)
 
     def run(self):
-        connection,x = self.database_socket.accept()
-       # self.server_socket.connect(self.address)
         while True:
             data = self.server_socket.recv(1024)
             if data:
+                self.database_socket.sendall(data)
                 # print(data.decode())
-                connection.sendall(data)
+                # connection.sendall(data)
                 message = 'Uspesno slanje'
                 message = message.encode()
                 self.server_socket.sendall(message)
-                data2 = connection.recv(1024)
+                # data2 = connection.recv(1024)
+                data2 = self.database_socket.recv(1024)
                 if data2:
                     print('Uspesno slanje')
                 else:

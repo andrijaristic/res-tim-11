04f31878bac1c386e31d02f4221d8ebc70c4c039
markomilenkovic00 <markomilenkovice14@gmail.com>
2022-05-29 15:49:10 +0200

    Kreiranje korisnickog interfejsa

---
+++ projekat/writer/classes.py
@@ -0,0 +1,32 @@
+import socket
+import datetime
+
+class Writer:
+    def __init__(self, server_adresa):
+        self.server_adresa = server_adresa
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def run(self):
+        while(True):
+            print("1 - Slanje podataka\n2 - Paljenje Worker komponente\n3 - Gasenje Worker komponente\n4 - Zatvaranje konekcije\nOdaberite komandu: ",end="")               
+            try:
+                komanda = int(input())
+                poruka = ""
+                if(komanda == 1):
+                    poruka = ""
+                elif(komanda == 2):
+                    poruka = ""
+                elif(komanda == 3):
+                    poruka = ""
+                elif(komanda == 4):
+                    poruka = ""
+                else:
+                    print("Zadata komanda ne postoji!")
+                   
+            except ValueError:
+                print("Morate uneti broj")
+            except:
+                print("Greska! Poruka nije uspesno poslata!")
+
+    
+
---
+++ projekat/writer/writer.py
@@ -0,0 +1,7 @@
+from classes import Writer
+
+if __name__ == '__main__':
+    server_adresa = ('localhost', 20000)
+    writer = Writer(server_adresa)
+    writer.run()
+         

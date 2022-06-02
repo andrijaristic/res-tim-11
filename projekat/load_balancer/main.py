b55181416c40fd764eb07e73fb75c98e95c7a5c3
filipilicpr1 <filipflyer98@gmail.com>
2022-06-02 15:56:46 +0200

    Promena naziva

---
+++ projekat/load_balancer/main.py
@@ -0,0 +1,29 @@
+from json import load
+from classes import LoadBalancer
+from threading import Thread
+
+if __name__ == '__main__':
+    try:
+        server_client_address = ('localhost',20000)
+        server_worker_address = ('localhost',21000)
+        local_buffer = []
+        client_connections = []
+        worker_connections = []
+        worker_availabilty = []
+        load_balancer = LoadBalancer()
+        client_socket = load_balancer.create_socket(server_client_address)
+        new_thread = Thread(target=load_balancer.start_listening_clients, args=(client_socket,client_connections,local_buffer,worker_connections,worker_availabilty))
+        new_thread.daemon = True
+        new_thread.start()
+        worker_socket = load_balancer.create_socket(server_worker_address)
+        worker_thread = Thread(target=load_balancer.start_listening_workers, args=(worker_socket,worker_connections,worker_availabilty))
+        worker_thread.daemon = True
+        worker_thread.start()
+        send_thread = Thread(target=load_balancer.check_data_for_sending,args=(local_buffer,worker_connections,worker_availabilty))
+        send_thread.daemon = True
+        send_thread.start()
+        end_thread = Thread(target=load_balancer.close_all_connections, args=(client_connections,worker_connections,worker_availabilty))
+        end_thread.start()
+    except:
+        print('Exception')
+        exit()

4eeb39891b14919d1d353a0ee5c1a7650ea5b498
jovan12121 <jovanmiladinovice14@gmail.com>
2022-06-18 00:05:30 +0200

    Update worker.py
    Refaktorizacija workera

--- projekat/worker/worker.py
+++ projekat/worker/worker.py
@@ -3,5 +3,8 @@ from classes import Worker
 if __name__ == '__main__':
     loadbalancer_address = ('localhost', 21000)
     databasecrud_address = ('localhost', 22000)
-    worker = Worker(loadbalancer_address,databasecrud_address)
+    worker = Worker()
+    worker.connecttodatabasecrud(databasecrud_address)
+    worker.connecttoloadbalancer(loadbalancer_address)
     worker.run()
+    

74e0d39691312f17112a3baf2adc44c359a7ef09
jovan12121 <jovanmiladinovice14@gmail.com>
2022-06-18 00:17:11 +0200

    Refaktorizacija workera

--- projekat/worker/classes.py
+++ projekat/worker/classes.py
@@ -1,30 +1,54 @@
 import socket
+from time import sleep
+from tkinter.tix import Tree
 
 
 class Worker:
-    def __init__(self, address, databasecrud_address):
-        self.address = address
-        self.databasecrud_address = databasecrud_address
+    def __init__(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.connect(address)
         self.database_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.database_socket.connect(databasecrud_address)
-
-    def run(self):
+    def connecttoloadbalancer(self,address): # pragma: no cover
+        try:
+            self.server_socket.connect(address)
+            return True
+        except:
+            return False
+    def connecttodatabasecrud(self,address): # pragma: no cover
+        try:
+            self.database_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.database_socket.connect(address)
+            return True
+        except:
+            return False
+    def run(self): # pragma: no cover
         while True:
-            data = self.server_socket.recv(1024)
+            data = self.receivedata(self.server_socket)
             if data:
+                self.checkdata(data)
                 self.database_socket.sendall(data)
-                # print(data.decode())
-                # connection.sendall(data)
-                message = 'Uspesno slanje'
-                message = message.encode()
-                self.server_socket.sendall(message)
-                # data2 = connection.recv(1024)
-                data2 = self.database_socket.recv(1024)
+                self.send('Upesno slanje',self.database_socket)
+                data2 = self.receivereply(self.database_socket)
                 if data2:
-                    print('Uspesno slanje')
-                else:
-                    print('Neuspesno slanje')
+                    print(data2)
             else:
                 exit()
+    def receivedata(self,sock):
+        data = sock.recv(1024)
+        return data
+    def checkdata(data):
+        s = data.decode()
+        if s.count(';')==10 and s.count('-')==20:
+            return True
+        else:
+            raise TypeError("Wrong format!")
+    def send(self,message,sock):
+        try:
+            message = message.encode()
+            sock.sendall(message)
+            return True
+        except:
+            return False
+    def receivereply(self,sock):
+        data = sock.recv(1024)
+        return data
+

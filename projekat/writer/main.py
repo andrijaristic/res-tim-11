f770931375adb9409fd2e8c16807d0f4631ffa3d
markomilenkovic00 <markomilenkovice14@gmail.com>
2022-06-17 23:44:11 +0200

    Refaktorizacija writer komponente

--- projekat/writer/main.py
+++ projekat/writer/main.py
@@ -2,6 +2,7 @@ from writer import Writer
 
 if __name__ == '__main__':
     server_adresa = ('localhost', 20000)
-    writer = Writer(server_adresa)
-    writer.run()
+    writer = Writer()
+    socket = writer.create_socket(server_adresa)
+    writer.run(socket)
          
--- projekat/writer/writer.py
+++ projekat/writer/writer.py
@@ -1,58 +1,83 @@
 import socket
 import datetime
 
-class Writer:
-    def __init__(self, server_adresa):
-        self.server_adresa = server_adresa
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(self.server_adresa)
+class Writer:      
+    def create_socket(self, adresa):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(adresa)
+        return sock
 
-# initial commit
+    def run(self, socket):
+        while(True):                                    
+            komanda = self.get_input("1 - Slanje podataka\n2 - Paljenje Worker komponente\n3 - Gasenje Worker komponente\n4 - Zatvaranje konekcije\nOdaberite komandu: ")
+            poruka = self.switch_komanda(komanda)               
+            self.send_message(poruka, socket)           
 
-    def run(self):
-        while(True):
-            print("1 - Slanje podataka\n2 - Paljenje Worker komponente\n3 - Gasenje Worker komponente\n4 - Zatvaranje konekcije\nOdaberite komandu: ",end="")               
-            try:
-                komanda = int(input())
-                poruka = ""
-                if(komanda == 1):
-                    poruka = self.input_and_validate_data()
-                elif(komanda == 2):
-                    poruka = "On-0"
-                elif(komanda == 3):
-                    poruka = "Off-0"
-                elif(komanda == 4):
-                    poruka = "Exit-0"
-                else:
-                    print("Zadata komanda ne postoji!")
-                if(poruka != ""):                                               
-                    poruka = poruka.encode()
-                    self.sock.sendall(poruka)
-                    odgovor = self.sock.recv(1024)
-                    if(odgovor):
-                        poruka = odgovor.decode()
-                        print(poruka)
-                    else:
-                        self.sock.close()
-                        break    
-            except ValueError:
-                print("Morate uneti broj")
-            except:
-                print("Greska! Poruka nije uspesno poslata!")
 
-    def input_and_validate_data(self):
+    def send_message(self ,poruka, socket):
+        if(type(poruka) != str):
+            raise TypeError("Poruka treba da bude string!")
         try:
-            print("Unesite ID brojila: ",end="")
-            id = int(input())
-            print("Unesite vrednost: ",end="")
-            value = int(input())
-            e = datetime.datetime.now()
-            datum_vreme = e.strftime("%d.%m.%Y;%H:%M:%S")
-            return "Send-" + str(id) + "-" + str(value) + "-" + str(datum_vreme)
-        except ValueError:
-            print("Greska. Vrednosti moraju biti celobrojne.")
-            return ""
+            if(poruka):                                               
+                poruka = poruka.encode()
+                socket.sendall(poruka)
+                odgovor = socket.recv(1024)
+                if(odgovor):
+                    poruka = odgovor.decode()
+                    print(poruka)
+                else:
+                    socket.close()
+                return True
+            else:
+                return False
         except:
             print("Greska!")
-            return ""
+            return False
+                
 
+    def switch_komanda(self, komanda):
+        if(type(komanda) != int):
+            raise TypeError("Komanda nije int!")
+        if(komanda == 1):
+            id = self.get_input("Unesite ID brojila: ")          
+            value = self.get_input("Unesite vrednost: ")
+            poruka = self.format_message(id, value)
+            return poruka
+        elif(komanda == 2):
+            poruka = "On-0"
+            return poruka
+        elif(komanda == 3):
+            poruka = "Off-0"
+            return poruka
+        elif(komanda == 4):
+            poruka = "Exit-0"
+            return poruka
+        else:
+            print("Zadata komanda ne postoji!")
+            return False
+
+
+    def format_message(self, id, value):
+        if(type(id) != int):
+            raise TypeError("ID nije int!")
+        if(type(value) != int):
+            raise TypeError("Vrednost nije int!")
+        try:                   
+            e = datetime.datetime.now()
+            datum_vreme = e.strftime("%d.%m.%Y;%H:%M:%S")
+            return "Send-" + str(id) + "-" + str(value) + "-" + str(datum_vreme)       
+        except:
+            print("Greska!")
+            return False
+
+
+    def get_input(self, text):
+        if(type(text) != str):
+            raise TypeError("Text treba da bude string!")
+        try:
+            return int(input(text))
+        except ValueError:
+            print("Greska. Vrednosti moraju biti celobrojne.")
+            return False
+
+    

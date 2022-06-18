9f6d9837b62aed6e05f394f923b217966e980a3f
jovan12121 <jovanmiladinovice14@gmail.com>
2022-06-18 17:24:42 +0200

    Ispravka bagova

--- projekat/tests/test_database_analitics.py
+++ projekat/tests/test_database_analitics.py
@@ -3,22 +3,22 @@ import os
 from unittest import mock
 from unittest.mock import Mock
 import unittest
-from database_analitics.classes import DatabaseAnalytics
+from database_analitics.database_analitics import DatabaseAnalytics
 class TestDatabasaAnalitics(unittest.TestCase):
-    def test_kreirajnazivfajlagrad(self):
+    def test_kreiraj_naziv_fajla_grad(self):
         analitics = DatabaseAnalytics()
-        nazivfajla = analitics.kreirajnazivfajlagrad('Novi Sad')
+        nazivfajla = analitics.kreiraj_naziv_fajla_grad('Novi Sad')
         self.assertEqual(nazivfajla,'NoviSadizvestaj.txt')
-        nazivfajla = analitics.kreirajnazivfajlagrad('beograd')
+        nazivfajla = analitics.kreiraj_naziv_fajla_grad('beograd')
         self.assertEqual(nazivfajla,'beogradizvestaj.txt')
-    def test_upisiufajlgrad(self):
+    def test_upisi_u_fajlgrad(self):
         analitics = DatabaseAnalytics()
-        f = analitics.upisiufajlgrad('fajl.txt','grad','1-1;2-2;3-3'.encode())
+        f = analitics.upisi_u_fajl_grad('fajl.txt','grad','1-1;2-2;3-3'.encode())
         self.assertTrue(filecmp.cmp('fajl.txt','tests/testgrad.txt',shallow=False))
         os.remove('fajl.txt')
-    def test_upisiufajlbrojilo(self):
+    def test_upisi_u_fajl_brojilo(self):
         analitics = DatabaseAnalytics()
-        f = analitics.upisiufajlbrojilo('fajl2.txt','1','1-1;2-2;3-3'.encode())
+        f = analitics.upisi_u_fajl_brojilo('fajl2.txt','1','1-1;2-2;3-3'.encode())
         self.assertTrue(filecmp.cmp('fajl2.txt','tests/testbrojilo.txt',shallow=False))
         os.remove('fajl2.txt') 
 
--- projekat/tests/test_worker.py
+++ projekat/tests/test_worker.py
@@ -2,23 +2,23 @@ from unittest import mock
 from unittest.mock import Mock
 import unittest
 import socket
-from worker.classes import Worker
+from worker.worker import Worker
 
 class TestWorker(unittest.TestCase):
-    def test_checkdata(self):
+    def test_check_data(self):
         worker = Worker()
-        self.assertRaises(TypeError,worker.checkdata,"sadsad".encode())
-        data = worker.checkdata(("10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;".encode()))
+        self.assertRaises(TypeError,worker.check_data,"sadsad".encode())
+        data = worker.check_data(("10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;10-10-10;".encode()))
         self.assertTrue(data,'Greska')
-    def test_receivedata(self):
+    def test_receive_data(self):
         worker = Worker()
         connection = Mock()
         connection.recv.return_value = "cao".encode()
-        self.assertAlmostEqual(worker.receivedata(connection),"cao".encode())
+        self.assertAlmostEqual(worker.receive_data(connection),"cao".encode())
         connection.recv.assert_called_once()
         connection.recv.assert_called_with(1024)
         connection.recv.return_value = "poruka".encode()
-        self.assertAlmostEqual(worker.receivedata(connection),"poruka".encode())
+        self.assertAlmostEqual(worker.receive_data(connection),"poruka".encode())
     def test_send(self):
         worker = Worker()
         adresa = ('localhost',25000)
@@ -26,11 +26,11 @@ class TestWorker(unittest.TestCase):
         data = 'poruka'
         self.assertAlmostEqual(worker.send(data,connection),True)
         connection.sendall.assert_called_once()
-    def test_receivereply(self):
+    def test_receive_reply(self):
         worker = Worker()
         connection = Mock()
         connection.recv.return_value = "poruka".encode()
-        self.assertAlmostEqual(worker.receivereply(connection),("poruka".encode()))
+        self.assertAlmostEqual(worker.receive_reply(connection),("poruka".encode()))
         connection.recv.assert_called_once()
         connection.recv.assert_called_with(1024)
         

937761a9a919110623669d846732d06397740d70
Marko Milenkovic <markomilenkovice14@gmail.com>
2022-06-19 12:16:51 +0200

    Ispravka bagova

--- projekat/writer/writer.py
+++ projekat/writer/writer.py
@@ -8,52 +8,46 @@ class Writer:
         return sock
 
     def run(self, socket):  # pragma: no cover
-        while(True):                                    
-            komanda = self.get_input("1 - Slanje podataka\n2 - Paljenje Worker komponente\n3 - Gasenje Worker komponente\n4 - Zatvaranje konekcije\nOdaberite komandu: ")
-            poruka = self.switch_komanda(komanda)               
-            self.send_message(poruka, socket)           
+        while(True): 
+            try:                                   
+                komanda = self.get_input("1 - Slanje podataka\n2 - Paljenje Worker komponente\n3 - Gasenje Worker komponente\n4 - Zatvaranje konekcije\nOdaberite komandu: ")
+                poruka = self.switch_komanda(komanda)               
+                self.send_message(poruka, socket)
+            except:
+                 print("Greska!")      
 
 
     def send_message(self ,poruka, socket):
         if(type(poruka) != str):
-            raise TypeError("Poruka treba da bude string!")
-        try:
-            if(poruka):                                               
-                self.send_nesto(poruka, socket)
-                odgovor = self.receive_data(socket)
-                if(odgovor):
-                    poruka = odgovor.decode()
-                    print(poruka)
-                else:
-                    self.close_socket(socket)
-                    exit()
-                return True
+            raise TypeError("Poruka treba da bude string!")       
+        if(poruka):                                               
+            self.send_nesto(poruka, socket)
+            odgovor = self.receive_data(socket)
+            if(odgovor):
+                poruka = odgovor.decode()
+                print(poruka)
             else:
-                return False
-        except:
-            print("Greska!")
-            return False
+                self.close_socket(socket)
+                exit()
+            return True
+        else:
+            return False       
     
     def receive_data(self,socket):
         return socket.recv(1024)
 
-    def close_socket(self,socket):
-        try:          
+    def close_socket(self,socket):               
             socket.close()
             return True
-        except:
-            return False
+        
 
     def send_nesto(self, poruka, socket):
         if(type(poruka) != str):
-            raise TypeError("Poruka treba da bude string")
-        try:
-            poruka = poruka.encode()
-            socket.sendall(poruka)
-            return True
-        except:
-            return False
-
+            raise TypeError("Poruka treba da bude string")       
+        poruka = poruka.encode()
+        socket.sendall(poruka)
+        return True
+        
     def switch_komanda(self, komanda):
         if(type(komanda) != int):
             raise TypeError("Komanda nije int!")
@@ -80,23 +74,15 @@ class Writer:
         if(type(id) != int):
             raise TypeError("ID nije int!")
         if(type(value) != int):
-            raise TypeError("Vrednost nije int!")
-        try:                   
-            e = datetime.datetime.now()
-            datum_vreme = e.strftime("%d.%m.%Y?%H:%M:%S")
-            return "Send-" + str(id) + "-" + str(value) + "-" + str(datum_vreme)       
-        except:
-            print("Greska!")
-            return False
+            raise TypeError("Vrednost nije int!")                       
+        e = datetime.datetime.now()
+        datum_vreme = e.strftime("%d.%m.%Y?%H:%M:%S")
+        return "Send-" + str(id) + "-" + str(value) + "-" + str(datum_vreme)       
+        
 
 
     def get_input(self, text):
         if(type(text) != str):
-            raise TypeError("Text treba da bude string!")
-        try:
-            return int(input(text))
-        except ValueError:
-            print("Greska. Vrednosti moraju biti celobrojne.")
-            return False
-
-    
+            raise TypeError("Text treba da bude string!")       
+        return int(input(text))
+       
